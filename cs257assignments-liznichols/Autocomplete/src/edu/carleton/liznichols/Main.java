94658b38c4591027239710122e9850ebf8786795
Liz Nichols <nicholsl@carleton.edu>
2018-03-27 20:39:33 -0500

    wrote a simple helloworld

--- cs257assignments-liznichols/Autocomplete/src/edu/carleton/liznichols/Main.java
+++ cs257assignments-liznichols/Autocomplete/src/edu/carleton/liznichols/Main.java
@@ -3,6 +3,6 @@ package edu.carleton.liznichols;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello, world!");
     }
 }
