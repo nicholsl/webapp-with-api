0e09e38872dcbdf9d544712549dbc9f9c884da6c
Liz Nichols <nicholsl@carleton.edu>
2018-04-16 21:06:21 -0500

    added api_test.py

---
+++ UsingAPIs/api_test.py
@@ -0,0 +1,108 @@
+""" Write a Python program called api_test.py that gives the user the ability to perform two
+    simple operations from the command line using one of your APIs:
+
+    (1) get a list of things, and
+    (2) get details about a single thing.
+
+    For example, suppose you were using an API about the US Senate. You might allow the user to
+    (1) get a list of all senators and
+    (2) given a senator's name (or ID number, or whatever the API uses to uniquely identify a senator),
+    print detailed information about that senator.
+
+    import requests
+
+url = "http://pokeapi.co/api/v2/pokemon/bulbasaur/"
+
+payload = ""
+response = requests.request("GET", url, data=payload)
+
+data = response.json()
+"""
+import requests
+import argparse
+
+abilities = []
+
+def get_abilities():
+    base = 'http://pokeapi.co/api/v2/ability'
+    ability = ""
+    response = requests.request("GET", base, data=ability)
+    data = response.json()
+    for item in data.get('results'):
+        abilities.append(item.get("name"))
+
+def get_data_for_ability(ability):
+    base = 'http://pokeapi.co/api/v2/ability/{0}'
+    ability_index = abilities.index(ability) + 1
+    url = base.format(ability_index)
+    description = ""
+    response = requests.request("GET", url, data=description)
+    data = response.json()
+    if data.get("effect_entries"):
+        ability_data = data.get("effect_entries")
+        return ability_data
+    else:
+        return data
+
+def main(args):
+    get_abilities()
+    if args.action == 'list':
+        print()
+        print()
+        print("Pokemon have the following abilities. "
+              "Abilities provide passive effects for Pokémon in battle or in the overworld. "
+              " Pokémon have multiple possible abilities but can have only one ability at a time. "
+              "Check out Bulbapedia for greater detail or run this program again with args 'specify' and a"
+              " particular ability to learn more about it.")
+        print()
+        print("*********")
+        print()
+        for item in abilities:
+            print(item)
+        print()
+        print("*********")
+        print()
+
+    elif args.action == 'specify':
+        if args.ability:
+            print()
+            print("The details for ", args.ability, "are: ")
+            print()
+            details = get_data_for_ability(args.ability)
+            # print(details[0].keys())
+            # print(details)
+            for key in details[0].keys():
+                if not(key =='language'):
+                    description = details[0][key]
+                    print(key, ' : ', description)
+                    print()
+            print("for a list of all abilities, run this program again with the arg 'list'")
+
+
+
+
+
+if __name__ == '__main__':
+    # When I use argparse to parse my command line, I usually
+    # put the argparse setup here in the global code, and then
+    # call a function called main to do the actual work of
+    # the program.
+    parser = argparse.ArgumentParser(description='Get ability info from the Pokemon api')
+
+    parser.add_argument('action',
+                        metavar='action',
+                        help='action to perform - list abilities or specify and investigate a specific ability',
+                        choices=['list', 'specify'])
+
+    parser.add_argument('ability',
+                        nargs = '?',
+                        metavar='ability',
+                        help ='type the ability you would like to investigate',
+                        default = None,
+                        choices=['stench', 'drizzle', 'speed-boost', 'battle-armor', 'sturdy', 'damp', 'limber',
+                                 'sand-veil', 'static', 'volt-absorb', 'water-absorb', 'oblivious', 'cloud-nine',
+                                 'compound-eyes', 'insomnia', 'color-change', 'immunity', 'flash-fire', 'shield-dust',
+                                 'own-tempo'])
+
+    args = parser.parse_args()
+    main(args)
