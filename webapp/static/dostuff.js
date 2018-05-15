d1d1770197edc0465f40ba8090921ba0492b0f99
Chiraag Gohel <cgohel@gmail.com>
2018-05-15 01:01:40 -0500

    stuff

--- webapp/static/dostuff.js
+++ webapp/static/dostuff.js
@@ -71,7 +71,7 @@ function onByIndustryButtonClicked() {
         for (var k = 0; k < industry_list.length; k++) {
             tableBody += '<tr>';
 
-            tableBody += '<td><a onclick="getIdentity(' 
+            tableBody += '<td><a onclick="getIdentity(' +  "')\">"
                 + industry_list[k]['industry'] + ', '
                 + industry_list[k]['industryID'] + '</a></td>';
 
