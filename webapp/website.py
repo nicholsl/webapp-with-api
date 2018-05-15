8e0f1e07585e233cfb339f68db774d2b97be5ad3
Chiraag Gohel <cgohel@gmail.com>
2018-05-15 17:25:11 -0500

    heh

--- webapp/website.py
+++ webapp/website.py
@@ -27,6 +27,11 @@ def get_main_page():
     global api_port
     return flask.render_template('index.html', api_port=api_port)
 
+@app.route('/identity')
+def get_something():
+	global api_port
+	return flask.render_template('industrytemplate.html', api_port=api_port)
+
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print('Usage: {0} host port api-port'.format(sys.argv[0]), file=sys.stderr)
