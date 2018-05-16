f6c72a806262503d6b25f7f6b72b2696d23cf946
Chiraag Gohel <cgohel@gmail.com>
2018-05-16 03:09:56 -0500

    yes

--- webapp/static/dostuff.js
+++ webapp/static/dostuff.js
@@ -259,11 +259,76 @@ function getIndustry(race_codes) {
         }
     })
 
+    $('#container').highcharts({
+        xAxis: {
+            categories: ["United States", "Western Europe", "Canada",
+                "Germany", "France", "Mexico"]
+        },
+        series: [{
+            data: [50, 100, 150, 200, 250, 300]
+        }]
+    });
+
+
+
 
         .catch(function(error) {
             console.log(error);
         });
     document.getElementById("race_distribution_div").style.display = "block";
+    var chartConfig = {
+        "type": "serial",
+        "dataProvider": [],
+        "valueAxes": [{
+            "gridColor": "#FFFFFF",
+            "gridAlpha": 0.2,
+            "dashLength": 0
+        }],
+        "gridAboveGraphs": true,
+        "startDuration": 1,
+        "graphs": [],
+        "chartCursor": {
+            "categoryBalloonEnabled": false,
+            "cursorAlpha": 0,
+            "zoomable": false
+        },
+        "categoryField": "category",
+        "categoryAxis": {
+            "gridPosition": "start",
+            "gridAlpha": 0
+        }
+    };
+
+    jQuery(document).ready(function() {
+        // get categories
+        jQuery('#data thead th').each(function(index) {
+            if (index) { // skip the first column
+                chartConfig.graphs.push({
+                    "balloonText": "[[category]]: <b>[[value]]</b>",
+                    "fillAlphas": 0.8,
+                    "lineAlpha": 0.2,
+                    "type": "column",
+                    "valueField": "value" + index
+                });
+            }
+        });
+
+        // get data points
+        jQuery('#data tbody tr').each(function(index) {
+            var dataPoint = {};
+            jQuery(this).find('th,td').each(function(index2) {
+                if (jQuery(this).is('th')) { // category
+                    dataPoint.category = this.innerHTML;
+                } else {
+                    dataPoint['value' + index2] = Number(this.innerHTML);
+                }
+            });
+            chartConfig.dataProvider.push(dataPoint);
+        });
+
+        // make the chart
+        var chart = AmCharts.makeChart("race_distribution_div", chartConfig);
+    });
 }
 
 // Get the container element
@@ -276,4 +341,7 @@ for (var i = 1; i < btnContainer.length; i+=2) {
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
     });
+
 }
+
+
