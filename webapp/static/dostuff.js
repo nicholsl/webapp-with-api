e9392747317b1a68ee6dd90967a7e7bd67e0c4ac
Liz Nichols <nicholsl@carleton.edu>
2018-05-15 19:04:33 -0500

    modified css and index.html to add a margin to the tables

--- webapp/static/dostuff.js
+++ webapp/static/dostuff.js
@@ -200,14 +200,11 @@ function getIndustry(industryID, industryName) {
 }
 
 // Get the container element
-var btnContainer = document.getElementsByClassName("btn-group");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.document.getElementsByClassName("btn");
+var btnContainer = document.getElementsByClassName("btn-group")[0]
 
 // Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
+for (var i = 0; i < btnContainer.length; i++) {
+    btnContainer[i].addEventListener("click", function() {
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
